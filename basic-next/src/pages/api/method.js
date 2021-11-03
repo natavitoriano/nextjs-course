3b342982ef23ef75b704be4dd2bb86d20481b582
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-11-03 10:46:48 -0300

    API class using nextjs part 2

---
+++ basic-next/src/pages/api/method.js
@@ -0,0 +1,10 @@
+export default (req, res) => {
+    if(req.method === "GET") {
+        res.status(200).json({ nome: "Pedro" })
+    } else {
+        res.status(200).json({ nome: "Maria" })
+    }
+    res.status(200).json({
+        method: req.method
+    })
+}
