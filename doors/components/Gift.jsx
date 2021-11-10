c896db515f7575dccfe639cb3820f796a6cc433e
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-11-10 14:58:28 -0300

    Aligning and putting gift body style

--- doors/components/Gift.jsx
+++ doors/components/Gift.jsx
@@ -3,8 +3,8 @@ import styles from "../styles/Gift.module.css";
 export default function Gift() {
     return(
         <div className={styles.gift}>
-            <div className={styles.cover}>Tampa</div>
-            <div className={styles.giftBody}>Corpo</div>
+            <div className={styles.cover}></div>
+            <div className={styles.giftBody}></div>
         </div>
     )
 }
