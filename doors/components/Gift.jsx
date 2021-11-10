8b4278598eec8cd8bc6b1a270e5f73ec35f6d349
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-11-10 15:15:01 -0300

    Adding gift tie, and their styles

--- doors/components/Gift.jsx
+++ doors/components/Gift.jsx
@@ -5,6 +5,8 @@ export default function Gift() {
         <div className={styles.gift}>
             <div className={styles.cover}></div>
             <div className={styles.giftBody}></div>
+            <div className={styles.tie1}></div>
+            <div className={styles.tie2}></div>
         </div>
     )
 }
