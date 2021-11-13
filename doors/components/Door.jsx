3988160832c3907a3bc0f7c7f6976be4042231f5
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-11-13 14:51:35 -0300

    Adding logic to check which door has been selected

--- doors/components/Door.jsx
+++ doors/components/Door.jsx
@@ -1,9 +1,11 @@
 import styles from "../styles/Door.module.css";
 
-export default function Door(){
+export default function Door(props){
+    const selected = props.selected ? styles.selected: ''
+
     return (
         <div className={styles.area}>
-            <div className={styles.frame}>
+            <div className={`${styles.frame} ${selected}`}>
                 <div className={styles.door}>
                     <div className={styles.number}>3</div>
                     <div className={styles.doorknob}></div>
