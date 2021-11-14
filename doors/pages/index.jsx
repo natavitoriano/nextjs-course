55f1c849aaaf61fc6e31791b850caec0fe7c5ecc
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-11-14 16:20:16 -0300

    Organizing index

--- doors/pages/index.jsx
+++ doors/pages/index.jsx
@@ -1,14 +1,13 @@
+import { useState } from "react";
 import Door from "../components/Door";
 import DoorModel from "../model/door";
 
 export default function Home() {
-  const d1 = new DoorModel(1, false, true)
-  const d2 = new DoorModel(2)
+  const [d1, setD1] = useState(new DoorModel(1))
 
   return (
     <div style={{display: "flex"}}>
       <Door door={d1}/>
-      <Door door={d2}/>
     </div>
   )
 }
