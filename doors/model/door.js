3a51df2c30c739772821f3e2fe638422ceb85e98
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-11-14 16:00:45 -0300

    Creating door class

---
+++ doors/model/door.js
@@ -0,0 +1,44 @@
+export default class Door {
+    #number
+    #haveGift
+    #selected
+    #open
+
+    constructor(number, haveGift = false, selected = false, open = false) {
+        this.#number = number
+        this.#haveGift = haveGift
+        this.#selected = selected
+        this.#open = open
+    }
+
+    get number(){
+        return this.#number
+    }
+
+    get haveGift(){
+        return this.#haveGift
+    }
+
+    get selected(){
+        return this.#selected
+    }
+
+    get open(){
+        return this.#open
+    }
+    
+    deselect() {
+        const selected = false;
+        return new Door(this.number, this.haveGift, selected, this.open)
+    }
+
+    toggleSelection() {
+        const selected = !this.selected;
+        return new Door(this.number, this.haveGift, selected, this.open)
+    }
+
+    opernDoor() {
+        const open = true;
+        return new Door(this.number, this.haveGift, this.selected, open)
+    }
+}
