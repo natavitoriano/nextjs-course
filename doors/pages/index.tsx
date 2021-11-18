de90a91731295878aa9f36bd00f19c8606eb39ff
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-11-18 20:32:28 -0300

    Testing function to create doors

--- doors/pages/index.tsx
+++ doors/pages/index.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import Door from "../components/Door";
+import { createDoors } from "../functions/doors";
 import DoorModel from "../model/door";
 
 export default function Home() {
-  const [d1, setD1] = useState(new DoorModel(1))
+  const [doors, setDoors] = useState(createDoors(3, 2));
+
+  function renderDoors() {
+    return doors.map(door => {
+      return <Door key={door.number} value={door} onChange={console.log}/>
+    })
+  }
 
   return (
     <div style={{display: "flex"}}>
-      <Door value={d1} onChange={newDoor => setD1(newDoor)}/>
+      {renderDoors()}
     </div>
   )
 }
