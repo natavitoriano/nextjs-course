49d98fa01c40ae0153af00d2925c45d79dd7a036
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-11-21 17:18:38 -0300

    Taking the content out of the index and putting it on a separate page called game

---
+++ doors/pages/game.tsx
@@ -0,0 +1,25 @@
+import styles from "../styles/Game.module.css";
+import Door from "../components/Door";
+import { useState } from "react";
+import { createDoors, updateDoors } from "../functions/doors";
+
+export default function game() {
+    const [doors, setDoors] = useState(createDoors(3, 2));
+
+  function renderDoors() {
+    return doors.map(door => {
+      return <Door key={door.num} value={door} 
+        onChange={newDoor => setDoors(updateDoors(doors, newDoor))}/>
+    })
+  }
+    return (
+        <div id={styles.game}>
+            <div className={styles.doors}>
+                {renderDoors()}
+            </div>  
+            <div className={styles.buttons}>
+
+            </div>
+        </div>
+    )
+}
--- doors/pages/index.tsx
+++ doors/pages/index.tsx
@@ -4,18 +4,9 @@ import { createDoors, updateDoors } from "../functions/doors";
 import DoorModel from "../model/door";
 
 export default function Home() {
-  const [doors, setDoors] = useState(createDoors(3, 2));
-
-  function renderDoors() {
-    return doors.map(door => {
-      return <Door key={door.num} value={door} 
-        onChange={newDoor => setDoors(updateDoors(doors, newDoor))}/>
-    })
-  }
-
   return (
     <div style={{display: "flex"}}>
-      {renderDoors()}
+      <h1>Initial form</h1>
     </div>
   )
 }
