a03ab540bee595651e0829c0a101860e89216971
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-11-25 21:28:11 -0300

    Adding css style to the card

--- doors/components/Card.tsx
+++ doors/components/Card.tsx
@@ -2,7 +2,11 @@ import styles from "../styles/Card.module.css";
 
 export default function Card(props) {
     return (
-        <div className={styles.card}>
+        <div className={styles.card}
+            style={{
+                backgroundColor: props.bgcolor?? "#fff"
+            }}
+            >
             Cartão
         </div>
     )
