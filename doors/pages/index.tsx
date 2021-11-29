6cdd8253e9b3ec3f6d4175d21afb167cb199fc36
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-11-29 14:42:30 -0300

    Adding number input to the form

--- doors/pages/index.tsx
+++ doors/pages/index.tsx
@@ -1,15 +1,22 @@
 import Card from "../components/Card";
 import styles from "../styles/Form.module.css";
 import Link from "next/link";
+import NumberInput from "../components/numberInput";
+import { useState } from "react";
 
 export default function Form() {
+  const [numberDoors, setNumberDoors] = useState(3);
+
   return (
     <div className={styles.form}>
       <div>
         <Card bgcolor="#c0392c">
           <h1>Monty Hall</h1>
         </Card>
-        <Card></Card>
+        <Card>
+          <NumberInput text="Qtde Portas?" 
+            value={numberDoors} onChange={newNumbers => setNumberDoors(newNumbers)} />
+        </Card>
       </div>
       <div>
         <Card></Card>
