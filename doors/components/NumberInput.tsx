801b73cdcb9c72c98e3c10e18b1e78cbe48f811c
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-12-04 18:13:42 -0300

    Adding increment and decrement buttons to doors, and adding style

--- doors/components/NumberInput.tsx
+++ doors/components/NumberInput.tsx
@@ -7,10 +7,16 @@ interface NumberInputProps {
 }
 
 export default function NumberInput(props) {
+    const dec = () => props.onChange(props.value - 1);
+    const inc = () => props.onChange(props.value + 1)
     return (
         <div className={styles.numberInput}>
             <span className={styles.text}>{props.text}</span>
             <span className={styles.value}>{props.value}</span>
+            <div className={styles.buttons}>
+                <button className={styles.btn} onClick={dec}>-</button>
+                <button className={styles.btn} onClick={inc}>+</button>
+            </div>
         </div>
     )
 }
